2e2e957e1ea54651f3e6fe47af9b20003fc659e2
Vishal Rastogi <maganuk@hotmail.com>
2023-08-22 03:35:01 +0530

    Added Oidc Callback Route

---
+++ src/views/OidcCallback.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import userManager from '@/user-manager';
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+onMounted(async () => {
+  try {
+    await userManager.signinRedirectCallback();
+    router.push('/');
+  } catch (err) {
+    console.log(err);
+  }
+});
+</script>
+
+<template>
+  <div></div>
+</template>
