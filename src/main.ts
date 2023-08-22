3b17d0b6c15f3306f6c6924a28f2ce5547f00852
Sujith K <sujithkofficial@gmail.com>
2023-08-22 13:09:17 +0530

    Inject User Globally

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import type { User } from 'oidc-client-ts';
+import { createApp, ref, type Ref } from 'vue'
 import App from './App.vue'
 import router from './router'
+import userManager from './user-manager';
 
 const app = createApp(App)
+
+const userRef = ref<User | undefined | null>(undefined);
+
+userManager.getUser().then(x => {
+    userRef.value = x;
+});
+
+userManager.events.addUserLoaded(x => {
+    userRef.value = x
+});
+
+app.provide('user', userRef);
 
 app.use(router)
 
